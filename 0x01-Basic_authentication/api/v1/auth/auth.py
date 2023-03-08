bdc77dfcc10c1cb652fb622027618c2670ac9700
codestronomer <johnrumide6@gmail.com>
2023-03-08 04:37:14 +0100

    [update] update require_auth method of auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,10 +12,18 @@ class Auth:
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """public method for auth class
+        """public method for auth class to check if path in excluded paths
         Return:
-            bool
+            bool: True if the path is not in excluded_paths, False otherwise
         """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            path + "/"
+            if excluded_paths.count(path) > 0:
+                return False
         return False
 
     def authorization_header(self, request=None) -> str:
