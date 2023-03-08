177a9c7a6f7cd0284a880d6cba46c2451adf6e3b
codestronomer <johnrumide6@gmail.com>
2023-03-08 05:53:21 +0100

    [feat] optimize require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,10 +29,15 @@ class Auth:
             return True
         if path in excluded_paths:
             return False
-        if not path.endswith("/"):
-            path = path + "/"
-        if path in excluded_paths:
-            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
