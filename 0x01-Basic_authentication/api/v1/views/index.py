dcf169d64c17819d04343331c3b36301cdd8dada
codestronomer <johnrumide6@gmail.com>
2023-03-08 03:56:09 +0100

    [feat] add new route and error handler for unauthorization

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def authorization() -> str:
+    """ GET /api/vi/unauthorized
+    Return: 
+      - raise a 401 error """
+    raise abort(401)
