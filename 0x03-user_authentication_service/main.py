d7ff2e5a9d6a2ff6496ee2298e332ff63b0c12fa
codestronomer <johnrumide6@gmail.com>
2023-03-16 00:25:44 +0100

    [feat] add tests to main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,67 +1,153 @@
 #!/usr/bin/env python3
 """
-Main file
+Test module for flask app
 """
 from db import DB
 from user import User
 
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-"""
-test_db = DB()
+import requests
 
-user_1 = test_db.add_user("test@test.com", "superhashedpwd")
-print(user_1.id)
 
-find_user = test_db.find_user_by(email="test@test.com")
-#print(find_user.id)
+def register_user(email: str, password: str) -> None:
+    """
+    Test to register a user with the given email and password.
+    Args:
+        email (str): Test user's email
+        password: Test user's password
+    Return:
+        None
+    """
+    resp = requests.post('http://127.0.0.1:5000/users',
+                         data={'email': email, 'password': password})
+    if resp.status_code == 200:
+        assert (resp.json() == {"email": email, "message": "user created"})
+    else:
+        assert(resp.status_code == 400)
+        assert(resp.json() == {"message": "email already registered"})
 
-try:
-    find_user = test_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
 
-try:
-    find_user = test_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    Test for log in with the given wrong credentials.
+    Args:
+        email: The email of the user.
+        password: The password of the user.
+    Returns:
+        None
+    """
+    r = requests.post('http://127.0.0.1:5000/sessions',
+                      data={'email': email, 'password': password})
+    assert (r.status_code == 401)
 
-try:
-    test_db.update_user(user_1.id, hashed_password="NewPwd")
-    print("Password updated")
-except ValueError:
-    print("Error")
 
-print(test_db.find_user_by(id=user_1.id).hashed_password)
-"""
+def profile_unlogged() -> None:
+    """
+    Test for profile without being logged in with session_id.
+    Returns:
+        None
+    """
+    r = requests.get('http://127.0.0.1:5000/profile')
+    assert(r.status_code == 403)
 
-from auth import Auth
 
-email = "me@me.com"
-password = "mySecurePwd"
+def log_in(email: str, password: str) -> str:
+    """
+    Test for log in with the given correct email and password.
+    Args:
+        email: The email of the user.
+        password: The password of the user.
+    Returns:
+        The session_id of the user.
+    """
+    resp = requests.post('http://127.0.0.1:5000/sessions',
+                         data={'email': email, 'password': password})
+    assert (resp.status_code == 200)
+    assert(resp.json() == {"email": email, "message": "logged in"})
+    return resp.cookies['session_id']
 
-auth = Auth()
-"""
-try:
-    user = auth.register_user(email, password)
-    print("Successfully created a new user!")
-except ValueError as err:
-    print("Cound not create a new user : {}".format(err))
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
-"""
+def profile_logged(session_id: str) -> None:
+    """
+    Test for profile with being logged in with session_id.
+    Args:
+        session_id: The session_id of the user.
+    Returns:
+        None
+    """
+    cookies = {'session_id': session_id}
+    r = requests.get('http://127.0.0.1:5000/profile',
+                     cookies=cookies)
+    assert(r.status_code == 200)
 
-auth.register_user(email, password)
-"""
-print(auth.valid_login(email, password))
-print(auth.valid_login(email, "WrongPwd"))
-print(auth.valid_login("unknown@email", password))
-"""
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+
+def log_out(session_id: str) -> None:
+    """
+    Test for log out with the given session_id.
+    Args:
+        session_id: The session_id of the user.
+    Returns:
+        None
+    """
+    cookies = {'session_id': session_id}
+    r = requests.delete('http://127.0.0.1:5000/sessions',
+                        cookies=cookies)
+    if r.status_code == 302:
+        assert(r.url == 'http://127.0.0.1:5000/')
+    else:
+        assert(r.status_code == 200)
+
+
+def reset_password_token(email: str) -> str:
+    """
+    Test for reset password token with the given email.
+    Args:
+        email: The email of the user.
+    Returns:
+        The reset_token of the user.
+    """
+    r = requests.post('http://127.0.0.1:5000/reset_password',
+                      data={'email': email})
+    if r.status_code == 200:
+        return r.json()['reset_token']
+    assert(r.status_code == 401)
+
+
+def update_password(email: str, reset_token: str,
+                    new_password: str) -> None:
+    """
+    Test for update password with the given email,
+    reset_token and new_password.
+    Args:
+        email: The email of the user.
+        reset_token: The reset_token of the user.
+        new_password: The new password of the user.
+    Returns:
+        None
+    """
+    data = {'email': email, 'reset_token': reset_token,
+            'new_password': new_password}
+    r = requests.put('http://127.0.0.1:5000/reset_password',
+                     data=data)
+    if r.status_code == 200:
+        assert(r.json() == {"email": email, "message": "Password updated"})
+    else:
+        assert(r.status_code == 403)
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
