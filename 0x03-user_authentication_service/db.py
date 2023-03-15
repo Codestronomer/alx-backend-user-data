4329c00e6031da65e0fe2dfcb20f87a74119d4d7
codestronomer <johnrumide6@gmail.com>
2023-03-15 02:03:05 +0100

    [fix] db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -13,13 +13,11 @@ from user import Base, User
 
 
 class DB:
-    """
-    DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """
-        Initialize a new DB Instance
+        """Initialize a new DB instance.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -28,8 +26,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """
-        Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
