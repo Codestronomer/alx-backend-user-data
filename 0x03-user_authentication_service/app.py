ea34804d5f492647d6e93926635aa95d486665d1
codestronomer <johnrumide6@gmail.com>
2023-03-15 22:16:27 +0100

    [feat] add login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,13 @@
 """
 Entry file for flask app
 """
-from flask import Flask, jsonify, request
+from flask import (
+        Flask,
+        jsonify,
+        request,
+        make_response,
+        abort
+        )
 from auth import Auth
 
 # initialize flask app
@@ -20,6 +26,7 @@ def home():
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def register_user():
+    """ User registration route """
     email, password = request.form['email'], request.form['password']
     try:
         user = AUTH.register_user(email, password)
@@ -27,6 +34,23 @@ def register_user():
             return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """user login route"""
+    email, password = request.form['email'], request.form['password']
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            resp = make_response(jsonify({"email": email,
+                                          "message": "logged in"}))
+            resp.set_cookie("session_id", session_id)
+            return resp
+        else:
+            return abort(401)
+    except Exception as err:
+        return err
 
 
 if __name__ == "__main__":
