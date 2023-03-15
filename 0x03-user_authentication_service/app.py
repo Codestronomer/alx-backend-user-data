3fa0d0877c5b963d8e8c26226f36eca9616c2f13
codestronomer <johnrumide6@gmail.com>
2023-03-15 23:56:59 +0100

    [feat] add password_reset route to app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,5 +78,17 @@ def profile():
     return abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """ Get reset password token """
+    email = request.form['email']
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
